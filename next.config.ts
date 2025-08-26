73c2afe6df72b97faa3514dd8df6416d77b0e56c
vincent vm <vincentvm1993@gmail.com>
2025-08-26 15:09:13 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,12 @@ const nextConfig: NextConfig = {
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'files.cdn.printful.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
