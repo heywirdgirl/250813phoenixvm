83c935cf935f614f47a63e71ab700f7770282271
vincent vm <vincentvm1993@gmail.com>
2025-09-03 20:00:07 +0000

    Attempted import error: 'googleCloud' is not exported from '@genkit-ai/f

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,8 +1,7 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import { googleCloud } from '@genkit-ai/firebase';
 
 export const ai = genkit({
-  plugins: [googleAI(), googleCloud()],
+  plugins: [googleAI()],
   model: 'googleai/gemini-2.0-flash',
 });
